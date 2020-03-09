8978cdc804a2d169e4b9607d80744fe0d5772e84
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-03-09 10:39:29 +0530

    deleated emplty script file and folder

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let inputAmount = document.querySelector("#unitInput").innerHTML;
-
-console.log(inputAmount);
-document.querySelector("#convertFromKg").addEventListener("click", function() {
-  document.querySelector(".output").innerHTML = inputAmount;
-});
